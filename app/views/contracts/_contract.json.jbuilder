7ffbc746653c4bd757a6bc1d334e439d9a0d01ed
Odette Gleen <nkargleeny@gmail.com>
2023-02-19 09:10:52 +0100

    scaffold contracts

---
+++ app/views/contracts/_contract.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! contract, :id, :name, :amount, :author_id, :created_at, :updated_at
+json.url contract_url(contract, format: :json)
---
+++ db/migrate/20230219080357_create_contracts.rb
@@ -0,0 +1,11 @@
+class CreateContracts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contracts do |t|
+      t.string :name
+      t.integer :amount
+      t.references :author, null: false, foreign_key: {to_table: :users}
+
+      t.timestamps
+    end
+  end
+end
