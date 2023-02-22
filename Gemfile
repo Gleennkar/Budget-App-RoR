b6aa460b74a9af17beae7052a43c2afc2dbc9d05
Odette Gleen <nkargleeny@gmail.com>
2023-02-22 19:17:05 +0100

    set up authorization

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem "sprockets-rails"
 
 gem 'devise'
 
+gem 'cancancan'
+
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,21 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :authenticate_user!
   
+    private
+
+    def after_sign_out_path_for(_resoruce)
+      root_path
+    end
+  
+    def after_sign_in_path_for(_resoruce)
+      flash[:notice] = 'Welcome to BudgetMe!'
+      categories_path
+    end
+  
+    def after_sign_up_path_for(_resoruce)
+      flash[:notice] = 'Welcome! Your account has been created'
+      root_path
+    end
  
     protected
   
